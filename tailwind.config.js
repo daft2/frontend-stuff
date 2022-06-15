a0d125d3311c446ea90536f8b23404d5388a8ec7
daft2 <daftdevs@gmail.com>
2022-06-16 01:59:25 +0700

    chore: import assets & custom color/theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,17 @@ module.exports = {
     extend: {
       colors: {
         'draxThemeOrange': '#FEDCC5',
-        'draxBtnOrange': '#F25019'
+        'draxBtnOrange': '#F25019',
+        'neobrutalismDarkGreen': '#00AC83',
+        'neobrutalismGreen': '#0FF2BC',
+        'neobrutalismTheme': '#EEEDDE',
+        'neobrutalismText': '#1F1F1F'
       },
       spacing: {
         '45percent': '45%'
+      },
+      boxShadow: {
+        'neobrutalism': '3px 3px 1px black'
       }
     },
   },
