c5bc8af8890486c006c6f6939c068d5485809d4f
daft2 <daftdevs@gmail.com>
2022-07-31 01:08:28 +0700

    fix conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
       },
       spacing: {
         '45percent': '45%'
+      },
+      boxShadow: {
+        'neobrutalism': '3px 3px 1px black',
+        'checkbox': '2px 2px 1px black',
+        'around': '0 0 10px rgb(0 0 0 / 0.1)'
+      },
+      lineHeight: {
+        '1/10em':'0.1em'
       }
     },
   },
